9d2a8d03af49de622b19502cfe12c074e01519b9
Shabnam Emdadi <shab.emdadi@gmail.com>
2015-07-11 17:19:11 -0700

    working on twitter sign in

--- server.py
+++ server.py
@@ -10,6 +10,12 @@ from datetime import datetime, timedelta
 import time
 import ast
 import os
+import tweepy
+
+consumer_key=os.environ['TWITTER_CONSUMER_KEY']
+consumer_secret=os.environ['TWITTER_CONSUMER_SECRET']
+access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY']
+access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
 app = Flask(__name__)
 
@@ -123,6 +129,53 @@ def process_report():
         category_stripped = category.strip('"')
         category_list.append(category_stripped)
 
+
+@app.route('/twitter_auth', methods=["GET"])
+
+    # Example using callback (web app)
+    verifier = request.args.get('oauth_verifier')
+    # verifier = auth.request_token["oauth_token"]
+
+    print verifier
+
+    # Let's say this is a web app, so we need to re-build the auth handler
+    # first...
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    token = session.get('request_token')
+    session.clear()
+    # session.delete('request_token')
+    auth.request_token = token
+
+    try:
+        auth.get_access_token(verifier)
+    except tweepy.TweepError:
+        print 'Error! Failed to get access token.'
+
+    token = auth.access_token
+    secret = auth.access_token_secret
+
+    print token
+    print secret
+
+    # Construct the API instance
+    api = tweepy.API(auth)
+
+
+@app.route('/twitter_signin')
+def twitter_auth():
+    """Twitter authorization."""
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret,"https://isafe.herokuapp.com/twitter_auth")
+
+    # Redirect user to Twitter to authorize
+    try:
+        redirect_url = auth.get_authorization_url()
+    except tweepy.TweepError:
+        print 'Error! Failed to get request token.'
+
+    session.setdefault('request_token', auth.request_token)
+
+    print auth.request_token["oauth_token"]
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
